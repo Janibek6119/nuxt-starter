95210e970a32074aa96d92f700e676301c8819b3
Janibek6119 <eldar.malyshov@gmail.com>
2025-08-21 21:51:09 +0000

    tailwindcss-prettier-eslint (no tailwindcss-eslint yet)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex min-h-screen flex-col">
+    <div class="flex-center text-white">Hello World</div>
   </div>
 </template>
