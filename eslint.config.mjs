add98bb20fd25eece2760aa2e44dd22cfe82bfa4
Eldar Malyshov <eldar.malyshov@gmail.com>
2026-06-22 16:37:41 +0000

    eslint tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,36 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+import eslintParserVue from "vue-eslint-parser";
 
-export default withNuxt(eslintPluginPrettierRecommended, {
-  rules: {
-    "prettier/prettier": "warn",
-    "vue/component-name-in-template-casing": [
-      "warn",
-      "PascalCase",
-      {
-        registeredComponentsOnly: false,
+export default withNuxt(
+  eslintPluginPrettierRecommended,
+  {
+    extends: [eslintPluginBetterTailwindcss.configs.recommended],
+    languageOptions: {
+      parser: eslintParserVue,
+    },
+    files: ["**/*.vue"],
+    rules: {
+      "better-tailwindcss/enforce-consistent-line-wrapping": "off", // conflicts with / covered by prettier-plugin-classnames
+      "better-tailwindcss/enforce-consistent-class-order": "off", // conflicts with / covered by prettier-plugin-tailwindcss
+    },
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "app/assets/css/main.css",
       },
-    ],
+    },
   },
-});
+  {
+    rules: {
+      "prettier/prettier": "warn",
+      "vue/component-name-in-template-casing": [
+        "warn",
+        "PascalCase",
+        {
+          registeredComponentsOnly: false,
+        },
+      ],
+    },
+  },
+);
